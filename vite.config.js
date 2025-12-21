d9cc23c9c483e4344b9d2f0d0423717618ec824f
cristhi salgue <salcristhi5411@gmail.com>
2025-12-21 16:32:34 -0500

    chore: Full project cleanup and standardized build config (React 18)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'es2020',
+    outDir: 'dist',
     sourcemap: false,
-    minify: 'esbuild',
-  },
+    // Minimize bundle size
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          'router': ['react-router-dom'],
+          'analytics': ['react-ga4', 'react-facebook-pixel', 'facebook-nodejs-business-sdk'],
+          'ui': ['react-icons']
+        }
+      }
+    },
+    // Standard minification
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 });

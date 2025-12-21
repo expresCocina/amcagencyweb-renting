2c59e5e4bde44805c34ef6f89d0bf5cc69b41f83
cristhi salgue <salcristhi5411@gmail.com>
2025-12-21 16:18:04 -0500

    fix: Ultra-minimal config to resolve all build errors

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      fastRefresh: true,
-      babel: {
-        plugins: [
-          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug'],
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-core': ['react', 'react-dom'],
-          'react-router': ['react-router-dom'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
     sourcemap: false,
-  },
-  optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react-router-dom',
-    ],
+    minify: 'esbuild',
   },
 });

952c4f55b9dcc2c5bf3f13fbe549a75e953fb666
cristhi salgue <salcristhi5411@gmail.com>
2025-12-27 15:05:48 -0500

    Revert "perf: Optimize performance with lazy loading and code splitting"
    This reverts commit a0b2f9534347e98468b4097cacaf9b8146819466.

--- vite.config.js
+++ vite.config.js
@@ -8,23 +8,13 @@ export default defineConfig({
     sourcemap: false,
     emptyOutDir: true,
     manifest: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug']
-      }
-    },
     rollupOptions: {
       output: {
         manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'icons': ['react-icons/fa', 'react-icons/md', 'react-icons/bs'],
-          'supabase': ['@supabase/supabase-js']
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['react-icons']
         }
       }
-    },
-    chunkSizeWarningLimit: 1000
+    }
   }
 });
